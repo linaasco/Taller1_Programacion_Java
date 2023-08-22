0ff6de5362ea65508e33e8ff7718c878e50f6b5d
linaasco <linas.cortesh@konradlorenz.edu.co>
2023-08-22 18:05:47 -0500

    Codigo recibe de entrada fecha de cumpelaños y salida edad

---
+++ Edad.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int dia;
+        int mes;
+        int anno;
+        int dia1;
+        int mes1;
+        int anno1;
+        int resultado = 0;
+        int casos = 0;
+        System.out.println("Digite el dia de su cumpleaños");
+        dia = sc.nextInt();
+
+        System.out.println("Digite el mes de su cumpleaños");
+        mes = sc.nextInt();
+
+        System.out.println("Digite el anno de su cumpleaños");
+        anno = sc.nextInt();
+
+        System.out.println("Digite que dia es hoy");
+        dia1 = sc.nextInt();
+
+        System.out.println("Digite el mes de hoy");
+        mes1 = sc.nextInt();
+
+        System.out.println("Digite el anno en el que esta actualmente");
+        anno1 = sc.nextInt();
+
+        resultado = anno1 - anno;
+
+        if (mes>mes1){
+            casos= resultado-1;
+            System.out.println("Usted tiene " + casos + " annos");
+        }
+        else {
+            System.out.println("Usted tiene " + resultado + " annos");
+        }
+    }
+}
